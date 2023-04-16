3d0ef9e339e80cb11205ec38b5024d70d913c50c
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2023-04-16 18:57:50 +0530

    Update the flask app and html templates

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from pyspark.sql import SparkSession
 from pyspark.ml.pipeline import PipelineModel
 from pyspark.ml.tuning import CrossValidatorModel
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
 
 app = Flask(__name__)
@@ -23,54 +25,90 @@ spark = SparkSession\
 # Load the data prep, string indexer and ML models
 data_prep_model = PipelineModel.load("./model/8-classes/data_prep/")
 lr_model = CrossValidatorModel.load("./model/8-classes/logistic_regression")
+rf_model = CrossValidatorModel.load("./model/8-classes/random_forest")
 
 # Label map
 labels = ["pop", "country", "blues", "rock", "jazz", "reggae", "hip hop", "retro"]
-label_map = {0:"pop", 1:"country", 2:"blues", 3:"rock", 4:"jazz", 5:"reggae", 6:"hip hop", 7:"retro"}
+label_map = {0:"pop", 1:"country", 2:"blues", 3:"rock",
+             4:"jazz", 5:"reggae", 6:"hip hop", 7:"retro"}
 
 
 @app.route('/')
 def home():
     """
-    Home page
+        Home page
     """
     return render_template("home.html")
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/results', methods=['POST'])
+def results():
     """
-    Predict the genre of user input lyrics
+        Predict the genre of user input lyrics
     """
     # Fetch user input
     lyrics = request.form['lyrics']
+    model_name = request.form['model']
+
+    # Get the relavant model
+    if model_name=="logistic_regression":
+        model = lr_model
+    elif model_name=="random_forest":
+        model = rf_model
+
     input_df = spark.createDataFrame([("unknown", "unknown", "unknown", "unknown", str(lyrics))],
-                                     ["artist_name", "track_name", "release_date", "genre", "lyrics"])
+                                    ["artist_name", "track_name", "release_date", "genre", "lyrics"])
 
     # Data pre-processing
     preprocessed_df = data_prep_model.transform(input_df)
 
     # Model prediction
-    y_pred = lr_model.transform(preprocessed_df)
+    y_pred = model.transform(preprocessed_df)
     predicted_genre = label_map[int(y_pred.collect()[0]['prediction'])]
     prediction_probs = y_pred.collect()[0]["probability"]
 
     # Visualize the predictions
-    fig, ax = plt.subplots(figsize=(5, 5))
-    ax.pie(prediction_probs, labels=labels, startangle=90, autopct='%1.2f%%')
-    ax.axis('equal')
+    # Create pie chart of class probabilities
+    fig1, ax1 = plt.subplots()
+    ax1.pie(prediction_probs, labels=labels, autopct='%1.1f%%')
+    ax1.set_title('Class Probabilities')
+    pie_chart = plot_to_img(fig1)
+    plt.close(fig1)
 
-    # Save the chart to a buffer
-    buffer_ = io.BytesIO()
-    plt.savefig(buffer_, format='png')
-    buffer_.seek(0)
+    # Create bar chart of class probabilities
+    fig2, ax2 = plt.subplots()
+    ax2.bar(x=labels, height=prediction_probs)
+    ax2.set_title('Class Probabilities')
+    ax2.set_xlabel('Genre')
+    ax2.set_ylabel('Probability')
+    plt.xticks(rotation=45)
+    bar_chart = plot_to_img(fig2)
+    plt.close(fig2)
 
-    # Encode the buffer in base64
-    chart_url = base64.b64encode(buffer_.getvalue()).decode()
-    plt.close(fig)
+    return render_template('results.html',
+                           lyrics=lyrics,
+                           prediction=predicted_genre,
+                           pie_chart=pie_chart,
+                           bar_chart=bar_chart)
 
-    return render_template('results.html', lyrics=lyrics,
-                           prediction=predicted_genre, chart_url=chart_url)
+
+@app.route('/home')
+def go_home():
+    """
+        Return to home page
+    """
+    return render_template('home.html')
+
+
+def plot_to_img(plot):
+    """
+        Helper function to convert plot to image
+    """
+    img = io.BytesIO()
+    plot.savefig(img, format='png')
+    img.seek(0)
+    img = base64.b64encode(img.getvalue()).decode()
+    return "data:image/png;base64,{}".format(img)
 
 
 if __name__ == '__main__':
