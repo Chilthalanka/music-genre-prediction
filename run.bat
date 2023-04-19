b5ab4f24099022e357adce3a108c76ac7ce28843
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2023-04-19 12:14:41 +0530

    Download only wordnet data and set the PYSPARK_PYTHON environment variable

--- run.bat
+++ run.bat
@@ -1,26 +1,25 @@
-@echo off
+echo on
 
 :: Create the virtual environment cwij_msc_env
 echo Creating virtual environment...
-
 python -m venv cwij_msc_env
 
 :: Activate the virtual environment cwij_msc_env
 echo Activating virtual environment...
-
-cwij_msc_env\Scripts\activate
+call cwij_msc_env\Scripts\activate.bat
 
 :: Install the required packages
 echo Installing required packages...
-
 pip install -r requirements.txt
 
 :: Download NLTK data
 echo Downloading NLTK data...
+python -m nltk.downloader wordnet
 
-python -m nltk.downloader all
+:: Set PYSPARK_PYTHON environment variable
+echo Set PYSPARK_PYTHON=python
+set PYSPARK_PYTHON=python
 
 :: Run the flask app
 echo Starting Flask app...
-
 python app.py
